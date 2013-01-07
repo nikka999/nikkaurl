7bbe903a61fbef3ef47e02e5dcf248d07de55bfa
Nikka Ghalili <nikka999@gmail.com>
2013-01-07 22:10:52 +0800

    controller changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,15 @@
 class HomeController < ApplicationController
   def index
+    @all = Shortner.all
   end
 
   def create
+    @shortner = params[:short]
+    if Shortner.create(@shortner)
+      render :json => {:feedback => 'Short URL was generated', :status => 'success'}
+    else
+      render :json => {:feedback => 'Short URL could not be generated' :status => 'failure'}
+    end
   end
 
   def update
